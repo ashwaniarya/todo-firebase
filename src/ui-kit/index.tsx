139f1aaf42938684e7545bd292ee93515b5db2e8
Ashwani <ashwaniparker@gmail.com>
2021-10-19 02:50:27 +0530

    ui kit index file

---
+++ src/ui-kit/index.tsx
@@ -0,0 +1,7 @@
+export { default as Text } from './custom/Typography'
+export { default as Box } from './custom/Box'
+export { default as Checkbox } from './custom/Checkbox'
+export { default as Button } from './custom/Button'
+export { default as Modal } from './custom/Modal'
+
+export * from './images'
